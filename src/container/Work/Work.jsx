5f1b9eb4b19312f0c25bdd6e450f6d82962a07f4
Luca Gennaro <lucagennaro91@gmail.com>
2023-05-15 18:02:54 +0200

    skills section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
-import { AppWrap } from '../../wrapper'
+import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
 import './Work.scss'
+import Skills from '../Skills/Skills'
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All')
@@ -106,4 +107,4 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(MotionWrap(Work, 'app__works'), 'work', 'app__primarybg')
